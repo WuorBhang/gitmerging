1f86a1d4f0a9e9df14c6d0b80232164f492166f8
WuorBhang <uhuribhang211@gmail.com>
2024-12-25 11:52:43 +0300

    adding jinub branch files

---
+++ solutions/double_letter.py
@@ -0,0 +1,4 @@
+The plus symbol (+) is one of the most commonly used operators in Python. It is used to add two numbers together. 
+For example, 2 + 2 = 4. The plus symbol is also used to
+concatenate strings together. For example, "hello" + "world" = "helloworld".
+three being subtraction, multiplication and division.
