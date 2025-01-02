c30b8d37fd5d5d6bc81f690b1336f762e7e98b98
Wuor Bhang <uhuribhang211@gmail.com>
2025-01-02 17:17:38 +0300

    adding letter checkig in a word

---
+++ solutions/char_present_in_word.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+A module for determining whether the present of a str in in the given word:.
+
+Module contents:
+    - the present of a str in in the given word:
+
+Created on XX XX XX
+
+@author: WUOR BHANG GATWECH
+
+Challenge is from the Codewars website.
+
+Defining the present of a str in in the given word:
+
+"""
+
+def char_present_in_word(char, word):
+    """
+    checking if letter a is present in word or b is present in word.
+    
+    parameter:
+    
+    char: any character 
+    word: any word 
+    
+    return:
+    
+    boolean: True or False
+    
+    Example:
+    >>> is_char_present("a", "word")
+    False
+    >>> is_char_present("w", "word")
+    """
+    return char in word
---
+++ solutions/tests/test_char_present_in_word.py
@@ -0,0 +1,28 @@
+import unittest
+
+from solutions.char_present_in_word import char_present_in_word
+
+class TestCharPresentInWord(unittest.TestCase):
+    """checking present of a str in in the given word: mango"""
+    def test_letter_a_present(self):
+      actual = char_present_in_word("a", "mango")
+      expected = True
+      self.assertEqual(actual, expected)
+    
+    def test_letter_b_present(self):
+      """checking the present b in the given word: boy"""
+      actual = char_present_in_word("b", "boy")
+      expected = True
+      self.assertEqual(actual, expected)
+
+    def test_letter_c_present(self):
+      """checking the present c in the given word: cat"""
+      actual = char_present_in_word("c", "cat")
+      expected = True
+      self.assertEqual(actual, expected)
+      
+    def test_letter_d_present(self):
+      """checking the present d in the given word: dog"""
+      actual = char_present_in_word("d", "Hotel")
+      expected = False 
+      self.assertEqual(actual, expected)
